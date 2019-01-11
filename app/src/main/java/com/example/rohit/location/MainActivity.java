e199061358325ab8a57b8214f1d53018329f50fc
Rohit Martires <rohitmartires14@gmail.com>
2019-01-11 17:23:35 +0530

    location logger with working location updates using fusedlocationproviderclient with working multithreading for reciving loction updates and sequential location loding with handlers with UI

--- app/src/main/java/com/example/rohit/location/MainActivity.java
+++ app/src/main/java/com/example/rohit/location/MainActivity.java
@@ -1,25 +1,12 @@
 package com.example.rohit.location;
 
 import android.Manifest;
-import android.annotation.SuppressLint;
-import android.arch.lifecycle.ViewModelProvider;
-import android.content.Context;
-import android.content.Intent;
-import android.content.IntentSender;
 import android.content.pm.PackageManager;
 import android.location.Location;
-import android.location.LocationListener;
-import android.location.LocationManager;
-import android.nfc.Tag;
-import android.os.Build;
 import android.os.Handler;
-import android.os.HandlerThread;
 import android.os.Looper;
 import android.os.Message;
 import android.os.SystemClock;
-import android.provider.Settings;
-import android.support.annotation.NonNull;
-import android.support.annotation.RequiresApi;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -30,23 +17,13 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
-import com.google.android.gms.common.api.ResolvableApiException;
 import com.google.android.gms.location.FusedLocationProviderClient;
-import com.google.android.gms.location.LocationAvailability;
 import com.google.android.gms.location.LocationCallback;
 import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationResult;
 import com.google.android.gms.location.LocationServices;
-import com.google.android.gms.location.LocationSettingsRequest;
-import com.google.android.gms.location.LocationSettingsResponse;
-import com.google.android.gms.location.SettingsClient;
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.android.gms.tasks.Task;
-
-import java.security.Permission;
 import java.util.ArrayList;
-import java.util.Queue;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -58,12 +35,11 @@ public class MainActivity extends AppCompatActivity {
     protected static final int REQUEST_CHECK_SETTINGS = 0x1;
     private LocationCallback locationCallback;
     private Location location;
-    RecyclerView recyclerView;
-    RecycleViewAdapter adapter;
-    public Handler Handler;
-    public Worker worker;
-    ArrayList<Runnable> runnableArrayList;
-    boolean currentlocationrequestrecived=true;
+    private RecyclerView recyclerView;
+    private RecycleViewAdapter adapter;
+    public Handler uihandler;
+    public boolean recivedlocation = true;
+
 
     private static final String TAGt = "threadchecck";
 
@@ -82,56 +58,42 @@ public class MainActivity extends AppCompatActivity {
 
         button = findViewById(R.id.addLocationButton);
         recyclerView = findViewById(R.id.recyclerview);
-        runnableArrayList = new ArrayList<>();
-
 
         intRecyclerview();
 
-        worker = new Worker();
-        Log.d(TAGt, "Oncreate"+Looper.myLooper().getThread());
+        final Mythread mythread = new Mythread();
+        mythread.start();
+
 
         //addlocation-button
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
-                runnableArrayList.add(new Runnable() {
-
-
-                    @Override
-                    public void run() {
-
-                        intializelocaton();
-
-                        if (ActivityCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-                            return;
-                        }
-                        mfusedlocationprovierclient.requestLocationUpdates(locationRequest,locationCallback,null);
-                        Log.d("req-location updates", "Success");
-
-                        Log.d(TAGt, "run:"+Looper.myLooper().getThread());
-
-                    }
-                });
-
-                worker.execute();
-
+                if(recivedlocation==true) {
+                    mythread.getMhandler().sendEmptyMessage(0);
+                }else {
+                    Toast.makeText(getApplicationContext(),"Loading Previous Location",Toast.LENGTH_SHORT).show();
+                }
             }
         });
 
         //end button
 
-        Handler = new Handler(worker.getLooper()){
-            @Override
-            public void handleMessage(Message msg) {
-                super.handleMessage(msg);
-                Log.d("loc", "handleMessage:got ui");
-            }
-        };
+       uihandler= new Handler(Looper.getMainLooper()){
+           @Override
+           public void handleMessage(Message msg) {
+               super.handleMessage(msg);
+               Log.d("uithread", "handleMessage: ");
+               adapter.notifyDataSetChanged();
+           }
+       };
 
 
     }
 
+    public Handler getuihandler(){
+        return uihandler;
+    }
 
     private void intRecyclerview() {
 
@@ -156,19 +118,22 @@ public class MainActivity extends AppCompatActivity {
                     return;
                 }
                 else {
+                    //remove the loading
+                    longitude.remove(longitude.size()-1);
+                    latitude.remove(latitude.size()-1);
+                    //end remove
+
                     location=locationResult.getLocations().get(0);
                     longitude.add(String.valueOf(location.getLongitude()));
                     latitude.add(String.valueOf(location.getLatitude()));
-                    //adapter.notifyDataSetChanged();
 
                     mfusedlocationprovierclient.removeLocationUpdates(locationCallback);
                     Log.d("remove loc updates", "Success");
 
-                    currentlocationrequestrecived=true;
-                    runnableArrayList.remove(runnableArrayList.size()-1);
-                    worker.execute();
+                    getuihandler().sendEmptyMessage(0);
 
-                    Log.d(TAGt, "onLocationResult:"+Looper.myLooper().getThread());
+                    SystemClock.sleep(1000);
+                    recivedlocation=true;
 
                 }
 
@@ -185,37 +150,48 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-    class Worker extends HandlerThread{
-        private static final String TAG = "Worker";
 
-        Handler mhandler;
+    public class Mythread extends Thread{
+        public Handler mhandler;
 
-        public Worker() {
-            super(TAG);
-            start();
-            mhandler = new Handler(getLooper());
-            
-        }
 
-        public Worker execute(){
-
-            Log.d(TAG, "-------------------");
-            Log.d(TAG, "execute:start"+String.valueOf(runnableArrayList.size()));
-
-            if(currentlocationrequestrecived && runnableArrayList.size()!=0){
-            mhandler.post(runnableArrayList.get(runnableArrayList.size()-1));
-                Log.d(TAG, "execute:mid"+String.valueOf(currentlocationrequestrecived));
-                currentlocationrequestrecived=false;
-            }
-
-            Log.d(TAG, "execute: end");
-            return this;
+        public Handler getMhandler() {
+            return mhandler;
         }
 
         @Override
-        public boolean quit() {
-            Log.d(TAG, "worker-thread quit!!!");
-            return super.quit();
+        public void run() {
+
+            Looper.prepare();
+
+
+                mhandler = new Handler() {
+                    @Override
+                    public void handleMessage(Message msg) {
+                        super.handleMessage(msg);
+
+                        intializelocaton();
+
+                        if (ActivityCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                            return;
+                        }
+                        mfusedlocationprovierclient.requestLocationUpdates(locationRequest, locationCallback, null);
+                        Log.d("req-location updates", "Success");
+
+                        Log.d(TAGt, "run:" + Looper.myLooper().getThread());
+
+                        //put loading
+                        longitude.add("Loading...");
+                        latitude.add("Loading...");
+                        //end
+                        getuihandler().sendEmptyMessage(0);
+
+                        recivedlocation = false;
+                    }
+                };
+
+            Looper.loop();
+
         }
 
 
@@ -224,11 +200,4 @@ public class MainActivity extends AppCompatActivity {
 
 
 
-
-
-
-
-
-
-
-}
+    }
